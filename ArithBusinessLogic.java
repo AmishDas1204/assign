c04f6dba24d5cf439e40a7f6239131b35322e792
AmishDas1204 <amishanant13@gmail.com>
2018-10-17 10:42:54 +0530

    first commit

---
+++ ArithBusinessLogic.java
@@ -0,0 +1,31 @@
+package com.junit.egg;
+/**
+ * Created by ADMIN on 7/28/17.
+ */
+public class ArithBusinessLogic {
+    private int a, b;
+    public ArithBusinessLogic(int a, int b)
+    {
+    	System.out.println("ArithBusinessLogic() constructor...");
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add()
+    {
+    	System.out.println("int add()");
+        return a+b;
+    }
+
+    public int sub()
+    {
+    	System.out.println("int sub()");
+        return a-b;
+    }
+
+    public int mul()
+    {
+    	System.out.println("int mul()");
+        return a*b;
+    }
+}
